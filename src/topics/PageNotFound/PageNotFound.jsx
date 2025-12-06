46776c3096828c474eefe0f13fed0bf072be611f
Gu99k <gulloochandk@gmail.com>
2025-12-06 17:13:45 +0530

    Fix PageNotFound navigation

--- src/topics/PageNotFound/PageNotFound.jsx
+++ src/topics/PageNotFound/PageNotFound.jsx
@@ -5,6 +5,7 @@ function PageNotFound() {
   const navigate = useNavigate();
   const st = {
     margin: "auto",
+    cursor: "pointer",
   };
 
   return (
@@ -13,7 +14,7 @@ function PageNotFound() {
         style={st}
         src="https://cdni.iconscout.com/illustration/premium/thumb/404-error-page-not-found-3702346-3119137.png"
         alt="404page"
-        onClick={() => navigate(".")}
+        onClick={() => navigate("/")}
       />
     </div>
   );
