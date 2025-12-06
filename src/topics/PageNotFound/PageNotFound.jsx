8b9fb6419f5f1d5dd03990c575515fe77f5b166a
Gu99k <gulloochandk@gmail.com>
2025-12-06 14:45:44 +0530

    Fix PageNotFound navigation

--- src/topics/PageNotFound/PageNotFound.jsx
+++ src/topics/PageNotFound/PageNotFound.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+  const navigate = useNavigate();
   const st = {
     margin: "auto",
   };
 
   return (
     <div>
-      <Link to="/">
-        <img
-          style={st}
-          src="https://cdni.iconscout.com/illustration/premium/thumb/404-error-page-not-found-3702346-3119137.png"
-          alt="404page"
-        />
-      </Link>
+      <img
+        style={st}
+        src="https://cdni.iconscout.com/illustration/premium/thumb/404-error-page-not-found-3702346-3119137.png"
+        alt="404page"
+        onClick={() => navigate(".")}
+      />
     </div>
   );
 }
